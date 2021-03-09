d276f3cfe0da0b2d0fb0de6ffdb0a165f8dfdd8e
Enrique Flores <ehf.81@hotmail.com>
2021-03-08 23:27:15 -0800

    edit

--- js/ch15v5Probs.js
+++ js/ch15v5Probs.js
@@ -4,12 +4,20 @@ Information about an element
 
 const styleElement = getComputedStyle(document.getElementById("content"));
 const listElement = document.createElement("ul");
+
+// create and get the height info
 const heightElement = document.createElement("li");
 heightElement.textContent = `Height : ${styleElement.height}`;
+
+// create and get the width info
 const widthElement = document.createElement("li");
 widthElement.textContent = `Width: ${styleElement.width}`;
+
+// add width and height to the 'ul' tag
 listElement.appendChild(heightElement);
 listElement.appendChild(widthElement);
+
+// show output on page
 document
   .getElementById("infos")
   .appendChild(document.createTextNode("Information about the element"));
