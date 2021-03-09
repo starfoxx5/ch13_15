927ebc65e8081ade0de8e335332ed01f41ab3152
Enrique Flores <ehf.81@hotmail.com>
2021-03-09 00:17:56 -0800

    edit

--- js/hw6Prob4.js
+++ js/hw6Prob4.js
@@ -4,4 +4,25 @@
 const tblElement = document.createElement("table");
 
 // Loop to create rows and cells
-for (let i = 1)
+for (let i = 1; i <= 12; i++) {
+  // Create tr and td, assign i to td value
+  const trElement = document.createElement("tr");
+  const tdElement = document.createElement("td");
+  tdElement.innerText = i;
+
+  // Highlight every 4th number in the table
+  if (i % 4 === 0) {
+    tdElement.style.backgroundColor = "chartreuse"
+  }
+
+  // Add td to tr
+  trElement.appendChild(tdElement);
+
+  // Add tr to table
+  tblElement.appendChild(trElement);
+};
+
+// Add table to div tag
+// Get div tag and add tblElement to it
+const divElement = document.getElementById("output");
+divElement.appendChild(tblElement);
